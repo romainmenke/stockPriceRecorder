e25aa9761cfdcda6d9f0de9a5df79f658989d5b2
Romain Menke <romainmenke@gmail.com>
2016-04-14 13:01:05 +0200

    update

--- stockPriceRecorder.sh
+++ stockPriceRecorder.sh
@@ -1,19 +1,10 @@
-
-
-
 while :
 do
   price=$(curl "http://download.finance.yahoo.com/d/quotes.csv?s=KBC.BR&f=a")
-
   echo $price >> $RMBASHPATH/src/github.com/romainmenke/stockPriceRecorder/kbc.csv
-
   cd $RMBASHPATH/src/github.com/romainmenke/stockPriceRecorder
-
   git add .
-
   git commit -m "update"
-
   git push
-
   sleep 30
 done
